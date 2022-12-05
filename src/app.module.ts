e1d378331cf703c884016a1ba3264d4b0ee76177
Uladzislau Hasanau <u.hasanau@godeltech.com>
2022-12-05 12:20:28 +0100

    initialize telegram module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './database/database.module';
+import { TelegramModule } from './telegram/telegram.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ envFilePath: '.env' }), DatabaseModule],
+  imports: [
+    ConfigModule.forRoot({ envFilePath: '.env' }),
+    DatabaseModule,
+    TelegramModule,
+  ],
   controllers: [],
   providers: [],
 })
